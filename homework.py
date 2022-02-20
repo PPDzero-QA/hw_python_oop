782736260976b228c793204263a16f1179dd0092
Ruslan Sabirzyanov <dota.buy@yandex.ru>
2022-02-20 23:25:28 +0300

    Fix asdict

--- homework.py
+++ homework.py
@@ -10,12 +10,12 @@ class InfoMessage:
     distance: float
     speed: float
     calories: float
-    text_message = ('Тип тренировки: {}; Длительность: {:.3f} ч.;'
-                    ' Дистанция: {:.3f} км;'
-                    ' Ср. скорость: {:.3f} км/ч; Потрачено ккал: {:.3f}.')
+    text_message = ('Тип тренировки: {training_type}; Длительность: {duration:.3f} ч.;'
+                    ' Дистанция: {distance:.3f} км;'
+                    ' Ср. скорость: {speed:.3f} км/ч; Потрачено ккал: {calories:.3f}.')
 
     def get_message(self) -> str:
-        return self.text_message.format(*asdict(self).values())
+        return self.text_message.format(**asdict(self))
 
 
 class Training:
